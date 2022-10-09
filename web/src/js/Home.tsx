6a4f2d9249073bb23787849e897063d04c23c48f
juzybits <96890594+juzybits@users.noreply.github.com>
2022-10-09 01:50:41 +0000

    [web] youtube player proof of concept

--- web/src/js/Home.tsx
+++ web/src/js/Home.tsx
@@ -5,10 +5,24 @@ export function Home(props) {
         document.title = 'Journey';
     }, []);
 
+    var width = window.innerWidth // TODO: dynamic resize
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    var height = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
+
     return <section className='outlet'>
-        <h1>Journey</h1>
-        <div>
-            It's not about the destination.
-        </div>
+
+        <h1>Akira Bangers</h1>
+
+        <iframe id='video-iframe' width={width} height={height}
+            src='https://www.youtube.com/embed/UD0N7qjmrKY?controls=0&autoplay=1'
+            title='Journey video player' frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen>
+        </iframe>
+
     </section>;
 }
