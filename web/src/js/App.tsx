aac67f7f270dae35faad781e70ff574757554e62
juzybits <96890594+juzybits@users.noreply.github.com>
2023-02-15 23:05:55 -0100

    [web] disable network toggle now that testnet wave 2 is over

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -10,14 +10,12 @@ export function App()
         setNotification(text);
         setTimeout(() => { setNotification('') }, 1200);
     };
+    const network = 'devnet';
+    /*
+    // NOTE: getNetwork() and toggleNetwork() are duplicated in other Polymedia projects
 
     // Return either 'devnet' or 'testnet'
     const getNetwork = (): string => {
-        // TODO: remove this in a few days
-        const loc = window.location;
-        if (loc.hostname == 'testnet.polymedia-chat.pages.dev') {
-            loc.replace('https://chat.polymedia.app' + loc.pathname + '?network=testnet');
-        }
         // Read 'network' URL parameter
         const params = new URLSearchParams(window.location.search);
         // Delete query string
@@ -40,14 +38,13 @@ export function App()
         localStorage.setItem('polymedia.network', newNetwork);
         window.location.reload();
     };
-
-    // NOTE: getNetwork and toggleNetwork are duplicated in polymedia-gotbeef/web/src/js/App.tsx
+    */
 
     return <WalletKitProvider>
         {notification && <div className='notification'>{notification}</div>}
-        <div id='network-widget'>
+        {/*<div id='network-widget'>
             <a className='switch-btn' onClick={toggleNetwork}>{network}</a>
-        </div>
+        </div>*/}
         <Outlet context={[notify, network, connectModalOpen, setConnectModalOpen]} />
     </WalletKitProvider>;
 }
