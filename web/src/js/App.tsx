7c269adc36158f7207bc9aa3f94076d3c02f9eeb
juzybits <96890594+juzybits@users.noreply.github.com>
2023-04-04 03:41:09 +0000

    [web] add network selector via @polymedia/webutils

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { WalletKitProvider } from '@mysten/wallet-kit';
 
+import { NetworkSelector, currentNetwork as network } from '@polymedia/webutils';
+
 export function App()
 {
     const [connectModalOpen, setConnectModalOpen] = useState(false);
@@ -10,43 +12,10 @@ export function App()
         setNotification(text);
         setTimeout(() => { setNotification('') }, 1200);
     };
-    const network = 'localnet';
-    // Delete query string
-    window.history.replaceState({}, document.title, window.location.pathname);
-    /*
-    // NOTE: getNetwork() and toggleNetwork() are duplicated in other Polymedia projects
-
-    // Return either 'devnet' or 'testnet'
-    const getNetwork = (): string => {
-        // Read 'network' URL parameter
-        const params = new URLSearchParams(window.location.search);
-        // Delete query string
-        window.history.replaceState({}, document.title, window.location.pathname);
-        let newNetwork = params.get('network');
-        if (newNetwork === 'devnet' || newNetwork === 'testnet') {
-            // Update localStorage
-            localStorage.setItem('polymedia.network', newNetwork);
-            return newNetwork;
-        } else {
-            return localStorage.getItem('polymedia.network') || 'devnet';
-        }
-    };
-
-    const [network, setNetwork] = useState( getNetwork() );
-
-    const toggleNetwork = () => {
-        const newNetwork = network==='devnet' ? 'testnet' : 'devnet';
-        setNetwork(newNetwork);
-        localStorage.setItem('polymedia.network', newNetwork);
-        window.location.reload();
-    };
-    */
 
     return <WalletKitProvider>
         {notification && <div className='notification'>{notification}</div>}
-        {/*<div id='network-widget'>
-            <a className='switch-btn' onClick={toggleNetwork}>{network}</a>
-        </div>*/}
+        <NetworkSelector />
         <Outlet context={[notify, network, connectModalOpen, setConnectModalOpen]} />
     </WalletKitProvider>;
 }
